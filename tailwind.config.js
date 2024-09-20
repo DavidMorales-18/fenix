4d4219af921f50522ebbc94be56960b4ea9a6f99
DavidMorales-18 <bmoralest84@gmail.com>
2024-09-19 19:54:38 -0500

    pagina principal

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        'custom-blue': '#004D40',  // Color oscuro del uniforme para textos o fondos
+        'custom-lightblue': '#B2DFDB',  // Color claro para fondos o botones
+        'custom-teal': '#00796B',  // Otro tono para detalles o encabezados
+      }
+    }
+  },
+  variants: {},
+  plugins: [],
+}
